9960d300e64eef54e3eda1978ca20e3fcaa0a0a5
Digitexmedia <150277877+Digitexmedia@users.noreply.github.com>
2025-05-25 01:25:23 +0300

    Create config.php

---
+++ .gitignore/config.php
@@ -0,0 +1,6 @@
+<?php
+// config.php
+define('PESAPAL_CONSUMER_KEY', 'DvXS8AdJsMiVQXIc6O0c8bAhwT8GXGWM');
+define('PESAPAL_CONSUMER_SECRET', 'JIislJTT+AcAiPWTF408vvPO6Tw=');
+define('PESAPAL_IS_LIVE', false); // true if you go live
+?>
